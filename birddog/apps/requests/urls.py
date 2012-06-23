a41be7cf9b36caac3e8ed762a820ae0cc9f66338
Agustin Armendariz <aarmendariz@cironline.org>
2012-06-23 16:00:11 -0700

    editing request url and view hooked up with login

--- birddog/apps/requests/urls.py
+++ birddog/apps/requests/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls.defaults import *
 from apps.requests.views import RequestListView, RequestDetailView
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('',
     url(r'list/$', RequestListView.as_view(), name="request_list"),
+    url(r'detail/(?P<slug>.+)/edit/$', login_required(RequestDetailView.as_view()), name="request_edit"),
     url(r'detail/(?P<slug>.+)/$', RequestDetailView.as_view(), name="request_detail"),
+    
+    
 )
