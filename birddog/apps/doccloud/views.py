4cc791c565ebde0275ce4b34928eeb376c0ccc4e
Shane Shifflett <shifflett.shane@gmail.com>
2012-06-24 11:51:22 -0700

    new document views

--- birddog/apps/doccloud/views.py
+++ birddog/apps/doccloud/views.py
@@ -4,17 +4,27 @@ from django.conf import settings
 from django.template import RequestContext
 from django.shortcuts import get_object_or_404, render_to_response
 from apps.doccloud.forms import DocCloudDocForm
-from apps.doccloud.models import DocumentCloudProperties
+from apps.doccloud.models import DocumentCloudProperties, Document
 from django.contrib.auth.decorators import login_required
 
 @login_required
-def index(request, template_name='doccloud/upload.html'):
+def create(request, template_name='doccloud/upload.html'):
     context = {}
     context['form'] = DocCloudDocForm()
     return render_to_response(template_name, context, context_instance=RequestContext(request))
 
+def list(request, template_name='doccloud/list.html'):
+    context = {}
+    context['objects'] = Document.objects.all()
+    return render_to_response(template_name, context, context_instance=RequestContext(request))
+
+def detail(request, slug, template_name='doccloud/detail.html'):
+    context = {}
+    context['document'] = get_object_or_404(Document, slug=slug)
+    return render_to_response(template_name, context, context_instance=RequestContext(request))
+
 @login_required
-def upload(request, template_name='doccloud/complete.html'):
+def upload(request, template_name='doccloud/list.html'):
     context = {}
     try:
         if request.method == 'POST':
