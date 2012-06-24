d02dc3a9defc8fe7438dd375dd23529e6aa4b4b6
Michael Corey <mcorey@cironline.org>
2012-06-23 18:12:50 -0700

    Fixed some links

--- birddog/apps/agencies/models.py
+++ birddog/apps/agencies/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Agency(models.Model):
     name = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=30)
+    slug = models.SlugField(max_length=30,blank=True)
 
     class Meta:
         verbose_name_plural = 'Agencies'
 
     def __unicode__(self):
         return self.name
+        
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Agency, self).save(*args, **kwargs)
